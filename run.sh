8860ebd24fcc0e8c3c471703fdc54204aafc3a18
takiyu <takiyu1025.txu.development@gmail.com>
2023-06-11 06:32:42 +0000

    Update right bottom for space alignment

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # python ./keyboard_stl_generator.py -i ./layout_files/takiyu_left.json -r
-# python ./keyboard_stl_generator.py -i ./layout_files/takiyu_right.json -r
-python ./keyboard_stl_generator.py -i ./layout_files/takiyu_right_extra.json -r
+python ./keyboard_stl_generator.py -i ./layout_files/takiyu_right.json -r
+# python ./keyboard_stl_generator.py -i ./layout_files/takiyu_right_extra.json -r
