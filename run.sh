0d4f013f02b7c10569cc173985e1d440ab3f2a7e
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-03 12:43:08 +0000

    Add parameters file

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# python ./keyboard_stl_generator.py -i ./layout_files/takiyu_left.json -r
-python ./keyboard_stl_generator.py -i ./layout_files/takiyu_right.json -r
-# python ./keyboard_stl_generator.py -i ./layout_files/takiyu_right_extra.json -r
+python ./keyboard_stl_generator.py -i ./layout_files/takiyu_left.json -r -p ./layout_files/takiyu_parameters.json
+python ./keyboard_stl_generator.py -i ./layout_files/takiyu_right.json -r -p ./layout_files/takiyu_parameters.json
